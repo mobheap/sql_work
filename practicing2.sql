bb2c0c36456a4103038a303ee1fde7d5c5914bae
mobheap <abbassipro27@gmail.com>
2024-11-20 22:08:50 +0100

    more work

--- practicing2.sql
+++ practicing2.sql
@@ -218,3 +218,78 @@ FROM
     salaries
 WHERE
     from_date > '1997-01-01';
+USE employees;
+CREATE TABLE departments_dup (
+    dept_no CHAR(4),
+    dept_name VARCHAR(40)
+);
+insert into departments_dup
+SELECT dept_no, dept_name from departments;
+SELECT 
+    *
+FROM
+    departments_dup;
+insert into departments_dup
+values (null, 'Public Relations');
+DELETE FROM departments_dup 
+WHERE
+    dept_no = 'd002';
+insert into departments_dup(dept_no)
+values('d010'), ('d011');
+
+
+DROP TABLE IF EXISTS dept_manager_dup;
+CREATE TABLE dept_manager_dup (
+    emp_no INT(11) NOT NULL,
+    dept_no CHAR(4) NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NULL
+);
+INSERT INTO dept_manager_dup
+SELECT * from dept_manager;
+INSERT INTO dept_manager_dup (emp_no, from_date)
+VALUES                (999904, '2017-01-01'),
+						(999905, '2017-01-01'),
+						(999906, '2017-01-01'),
+						(999907, '2017-01-01');
+DELETE FROM dept_manager_dup 
+WHERE
+    dept_no = 'd001';
+INSERT INTO departments_dup (dept_name)
+VALUES               ('Public Relations');
+DELETE FROM departments_dup 
+WHERE
+    dept_no = 'd002';
+use employees;
+SELECT 
+    m.emp_no, m.dept_no, e.first_name, e.last_name, e.hire_date
+FROM
+    dept_manager m
+        JOIN
+    employees e ON m.emp_no = e.emp_no;
+select * from titles;
+SELECT 
+    e.first_name, e.last_name, e.hire_date, t.title
+FROM
+    employees e
+        JOIN
+    titles t ON e.emp_no = t.emp_no
+WHERE
+    e.first_name = 'Margareta'
+        AND e.last_name = 'Markovitch';
+
+SELECT 
+    e.first_name,
+    e.last_name,
+    e.hire_date,
+    t.title,
+    d.dept_name,
+    m.from_date
+FROM
+    employees e
+        JOIN
+    dept_manager m ON e.emp_no = m.emp_no
+        JOIN
+    departments d ON m.dept_no = d.dept_no
+        JOIN
+    titles t ON e.emp_no = t.emp_no;
