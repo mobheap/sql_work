f197fc836d584ae593642791dfbc846f31117f78
mobheap <abbassipro27@gmail.com>
2024-11-22 22:36:37 +0100

    work

--- practicing2.sql
+++ practicing2.sql
@@ -293,3 +293,15 @@ FROM
     departments d ON m.dept_no = d.dept_no
         JOIN
     titles t ON e.emp_no = t.emp_no;
+use employees;
+SELECT 
+    *
+FROM
+    dept_manager
+WHERE
+    emp_no IN (SELECT 
+            emp_no
+        FROM
+            employees
+        WHERE
+            hire_date BETWEEN '1990-01-01' AND '1995-01-01');
